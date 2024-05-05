242194ee2ffe515b12ec150557c788feb0ebdf65
Gurpreet2512Singh <84591513+gurpreetsingh1111@users.noreply.github.com>
2024-05-05 18:28:22 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import BlogEditor from './BlogEditor';
+
+function App() {
+  return (
+    <div>
+      <BlogEditor />
+    </div>
+  );
+}
+
+export default App;
